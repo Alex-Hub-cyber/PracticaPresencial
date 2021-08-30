fa6c873b81c910a7b685a6008a4389965feb91c3
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-08-30 08:13:19 -0500

    .
    .

---
+++ AppPresencial/AppPresencial/Controllers/EmpleadoController.cs
@@ -0,0 +1,54 @@
+﻿using AppPresencial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AppPresencial.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // GET: Empleado
+        public ActionResult Descripcion()
+        {
+            var empleados = new ClsEmpleado { Usuario = "Alex", DUI = 1111 };
+            
+            return View(empleados);
+        }
+
+        public ActionResult Editar(string Nombre, int Hora)
+        {
+            ClsEmpleado emp = new ClsEmpleado();
+            emp.Nombre = Nombre;
+            emp.Hora = Hora;
+
+            ViewBag.Nombe = Nombre;
+            ViewBag.Hora = Hora;
+
+            double U = 30;
+            double A = 1;
+            double DESCUENTO = 13;
+            double HORASTRABAJADAS = 2;
+
+            U* A;
+
+            
+           
+     
+            return View(emp);
+        }
+  
+        //private bool IsValid (string usuario,int DUI)
+        //{
+        //   // throw new NotImplementedException();
+        //}
+        //ViewBag.Usuario = "";
+        //ViewBag.DUI = "";
+
+        //return Redirect("/Empleado/Editar");
+    }
+
+       
+    }
---
+++ AppPresencial/AppPresencial/Models/ClsEmpleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppPresencial.Models
+{
+    public class ClsEmpleado
+    {
+        public string Usuario{ get; set; }
+
+        public int DUI{ get; set; }
+    }
+}
